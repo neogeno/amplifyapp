1a6d344b7f76e9b31b3810984cfb059617d95459
Chugh <parchugh@88665a14c197.ant.amazon.com>
2021-08-31 17:00:39 +0800

    Added Auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import Amplify, { Auth } from 'aws-amplify';
+
+
+Amplify.configure({
+  Auth: {
+    region: process.env.REACT_APP_REGION,
+    userPoolId: process.env.REACT_APP_USER_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+  }
+})
+
 
 function App() {
   return (
@@ -8,9 +20,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2!</h1>
+        <AmplifySignOut />
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App)
